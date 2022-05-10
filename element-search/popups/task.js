1a948398dd369334b71b6914df061294d04cf076
Чернова Александра <estelelendil@mail.ru>
2022-05-10 15:10:12 +0300

    Second Homework popup is done

---
+++ element-search/popups/task.js
@@ -0,0 +1,25 @@
+let buttoms = document.getElementsByTagName('a')
+
+let buttom = buttoms.item(1); //красная кнопка
+
+let modalSeccess = document.getElementById('modal_success') //выбираем зеленое окно
+
+buttom.onclick = function(){ // при нажатии добавляем класс, делающий видимым зеленое окно
+    
+    modalSeccess.classList.add('modal_active')
+}
+
+let closes = document.getElementsByClassName('modal__close modal__close_times');// коллекция элементов-крестиков(2)
+let modalMain = document.getElementById('modal_main') //выбираем красное окно
+
+
+closes[1].onclick = function(){
+    modalSeccess.classList.remove('modal_active')
+    modalMain.classList.remove('modal_active')
+    
+    
+}
+closes[0].onclick = function(){
+    modalMain.classList.remove('modal_active')
+    
+}
