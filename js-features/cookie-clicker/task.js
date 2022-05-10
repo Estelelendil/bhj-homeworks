ac6dede029507e5432ad4a290342cdb37ca65ef8
Чернова Александра <estelelendil@mail.ru>
2022-05-10 12:46:58 +0300

    CookieData added

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,10 +4,17 @@ console.log(cookie)
 let clickerCounter = 0;
 let clickerSpeed = document.getElementById('clicker__speed')
 let counterSpeed = 0;
+let timeNow = new Date;
+console.log(timeNow)
 
 cookie.onclick = function(){
 
 clickerCounter+=1;
+let timeCurrent = new Date;
+let delta = (timeCurrent - timeNow) / 1000;//секунды
+let speed = 1 / delta;
+clickerSpeed.textContent = speed.toFixed(2);
+timeNow = new Date;
 status.textContent = clickerCounter;
 cookie.width = 250;
 counterSpeed+=1; // количество кликов увеличивается при каждом нажатии
@@ -19,9 +26,9 @@ setTimeout(changeSize, 50)
 }
 
 // let start = Date.now();
-setInterval(function() {
+// setInterval(function() {
     
-    let speed = counterSpeed / 1; // количество кликов в течении этой секунды
-    clickerSpeed.textContent = speed;
-    counterSpeed = 0;
-}, 1000); 
+//     let speed = counterSpeed / 1; // количество кликов в течении этой секунды
+//     clickerSpeed.textContent = speed;
+//     counterSpeed = 0;
+// }, 1000); 
