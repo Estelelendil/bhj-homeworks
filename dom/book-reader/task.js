59f408d5154902d5d48254c9b832cacfb3fceb7c
Чернова Александра <estelelendil@mail.ru>
2022-05-19 20:03:34 +0300

    HW Dom is fixed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,21 +1,25 @@
 const btns = Array.from(document.querySelectorAll('.font-size'));
-console.log(btns)
-const book = document.getElementById('book')
-console.log(book)
+console.log(btns);
+const book = document.getElementById('book');
+console.log(book);
 
 btns.forEach(element =>{
+
     element.onclick = function(){
-        element.classList.add('font-size_active')
-        let atr = element.getAttribute('data-size')
-        book.classList.toggle('book_fs-'+atr)
-        console.log('book_fs-'+atr)
+
+        btns.forEach(btn=> btn.classList.remove('font-size_active'))
+        this.classList.add('font-size_active');
+        const atr = this.getAttribute('data-size');
+        book.className = atr ? 'book book_fs-'+atr : 'book';
         return false
     }
 })
 
-const colorTextParent = document.querySelector('.book__control_color')
-const colorText = colorTextParent.querySelectorAll('.color')
-console.log(colorText)
-const colorBgParent = Array.from(document.querySelector('.book__control_background'))
-const colorBg = Array.from(colorBgParent.querySelectorAll('.color'))
-console.log(colorBg)
+
+const colorTextParent = document.querySelector('.book__control_color');
+const colorText = colorTextParent.querySelectorAll('.color');
+console.log(colorText);
+const colorBgParent = Array.from(document.querySelector('.book__control_background'));
+const colorBg = Array.from(colorBgParent.querySelectorAll('.color'));
+console.log(colorBg);
+
