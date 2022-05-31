124d8e19692c7b26c308a57043a8ce70b11cb72f
Чернова Александра <estelelendil@mail.ru>
2022-05-31 18:50:32 +0300

    HW document-structure task tooltip is done

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,25 @@
+const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
+console.log(tooltips);
+
+tooltips.forEach(item =>{
+    item.addEventListener('click', (event)=>{
+        event.preventDefault();
+        const {top, left, height} = item.getBoundingClientRect();
+
+        let localTooltip = item.querySelector('.tooltip');
+        if(localTooltip){
+            localTooltip.remove();
+            return;
+        }
+    
+ let tooltip = document.querySelector('.tooltip');
+
+        if(tooltip){ 
+            tooltip.remove();
+        }
+
+        tooltipText = item.title;
+            item.insertAdjacentHTML('afterBegin', `<div class="tooltip tooltip_active" style="left: ${left}px; top: ${top + height}px">${tooltipText}</div>`);
+         
+    })
+})
