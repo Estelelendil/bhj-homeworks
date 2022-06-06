057b817bdd2bdcdf12083aba905e709b064ffc7d
Чернова Александра <estelelendil@mail.ru>
2022-06-06 17:24:44 +0300

    HW async

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,19 @@
 const progress = document.getElementById( 'progress' );
 progress.value = 0.0;
+const btn = document.querySelector('#send');
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
-xhr.send();
+btn.onclick = function(){xhr.send()};
 
 // xhr.addEventListener('readystatechange', ()=>{
 //     if(xhr.readyState == xhr.DONE){
 
 //     }
 // })
-xhr.upload.onprogress = function(event) {
-    console.log(event.loaded);
-    console.log(event.total)
-    alert( 'Загружено на сервер ' + event.loaded + ' байт из ' + event.total );
-  }
+xhr.upload.addEventListener('progress',(event)=>{
+  console.log(event.loaded);
+  console.log(event.total)
+  alert( 'Загружено на сервер ' + event.loaded + ' байт из ' + event.total );
+
+}) 
